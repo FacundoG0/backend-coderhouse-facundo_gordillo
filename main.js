d78d244746e70eeda1182f5c0385b2dbb84a0e2e
Facundo Gordillo <gordillofacundo44@gmail.com>
2023-01-20 09:45:18 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 class ProductManager {
-    constructor(title, description, price, thumbnail, code, stock){
-        this.title = title;
-        this.description = description;
-         this.price = price;
-         this.thumbnail = thumbnail;
-         this.code = code;
-         this.stock = stock;
-
+    constructor(products){
+        this.products = products;
+        products = ([])
     }
 
-    addProducts(){
-        let products = [];
+    addProducts(title, description, price, thumbnail, code, stock, id){
 
-        products.push({
-            title: this.title,
-            description: this.description,
-            price: this.price,
-            thumbnail: this.thumbnail,
-            code: this.code,
-            stock: this.stock,       
-        });
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+        
+        this.addProducts(id) = this.products.length + 1
     }
 }
 
-let limpiavidrios = new ProductManager("LIMPIAVIDRIOS", "LIMPIAVIDRIOS CREADO A BASE DE ACETONA Y ALCOHOL - INDUSTRIA ARGENTINA", "$450", "USUARIO/IMAGES", "1", "25")
+new ProductManager("LIMPIAVIDRIOS", "LIMPIAVIDRIOS CREADO A BASE DE ACETONA Y ALCOHOL - INDUSTRIA ARGENTINA", "$450", "USUARIO/IMAGES", "1", "25");
 
-let tractorsito = new ProductManager("TRACTORSITO", "JUGUETE DE PLÁSTICO PARA ÑINOS - CONTIENE PIEZAS PEQUEÑAS", "$850", "USUARIO/IMAGES", "2", "17")
+console.log(ProductManager.products)
