c5b49f795109430a9ece69ea0de40342b1ec8bd3
Facundo Gordillo <gordillofacundo44@gmail.com>
2023-01-23 10:49:03 -0300

    correcciones

--- main.js
+++ main.js
@@ -14,9 +14,33 @@ class ProductManager {
         
         for (let i = 0; i < products.length; i++) {
             products = products.push(product);
+
+            this.id = product.length
         }
 
+
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            console.log("Todos los campos son requeridos"); return;
+        }
     }
+
+    getProduct(code){
+        return this.products.find(product => product.code === code);
+    };
+
+    getProductById(id){
+        let product = this.products.find(product => product.id === id)
+        
+        if (!product){
+            console.log("No fue encontrado");
+        } 
+        
+        return product;
+    };
+
+    getAllProducts(){
+        return this.products;
+    }    
 }
 
 let product = new ProductManager("LIMPIAVIDRIOS", "LIMPIAVIDRIOS CREADO A BASE DE ACETONA Y ALCOHOL - INDUSTRIA ARGENTINA", "$450", "USUARIO/IMAGES", "1", "25");
